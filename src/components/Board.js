ac082785378b17f837c9d0a1f8f13ab162c1b4fb
Nikhil Dhumal <pekacoclover@gmail.com>
2023-03-28 21:52:10 +0530

    first commit

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Square from "./Square";
 import DataContext from "../context/DataContext";
 
@@ -9,8 +9,8 @@ const Board = () => {
         declareWinner,
         statusColor,
         isSeries,
-        numberOfMatches, setNumberOfMatches,
-        setWinDecider, winDecider,
+        numberOfMatches,
+        setWinDecider,
         tableData, setTableData,
         matchNumber, setMatchNumber } = useContext(DataContext)
     const [status, setStatus] = useState(null)
@@ -18,7 +18,7 @@ const Board = () => {
 
     useEffect(() => {
         check()
-    }, [squares])
+    }, [])
 
     const check = () => {
         const winner = declareWinner()
